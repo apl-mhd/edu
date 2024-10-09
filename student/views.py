0b4986d455c4ab2cc56a4e6c4774238a4c0298d4
Apel Mahmud <apl.mhd@gmail.com>
2024-10-09 15:27:48 +0600

    base template added

--- student/views.py
+++ student/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+from pathlib import Path
+import os
+
 
 # Create your views here.
+
+
+def index(request):
+
+    BASE_DIR = Path(__file__).resolve().parent.parent
+
+    a = f"{BASE_DIR}/templates/index.html"
+
+    return render(request, 'index.html')
