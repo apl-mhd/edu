83505f5bf78bcc92609f36f5950016c9610d1a68
Apel Mahmud <apl.mhd@gmail.com>
2025-03-19 23:35:25 +0600

    Refatore: Course module code refactore

--- course/urls.py
+++ course/urls.py
@@ -1,10 +1,9 @@
-from django.urls import path, include
-from .views import PaymentView, CourseAssingView, StudentPaymentListView
+from django.urls import path
 from . import apiviews
 urlpatterns = [
     path('', apiviews.CourseAPIView.as_view(), name='courses'),
-    path('payment/', PaymentView.as_view(), name='payment'),
-    path('course-assign/', CourseAssingView.as_view(), name='course-assign'),
-    path('student-payment-list/<int:id>/', StudentPaymentListView.as_view(),
+    path('payment/', apiviews.PaymentView.as_view(), name='payment'),
+    path('course-assign/', apiviews.CourseAssingView.as_view(), name='course-assign'),
+    path('student-payment-list/<int:id>/', apiviews.StudentPaymentListView.as_view(),
          name='student-payment-list'),
 ]
